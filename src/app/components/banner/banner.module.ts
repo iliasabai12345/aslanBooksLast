fe75482614aff89e3d09f6edeefab3c45c9353fc
iliasabai123 <ilias.abai.alser@gmail.com>
2022-12-24 17:52:52 +0600

    fix banner bug

--- src/app/components/banner/banner.module.ts
+++ src/app/components/banner/banner.module.ts
@@ -2,13 +2,15 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {BannerComponent} from "./banner.component";
 import {SwiperModule} from "swiper/angular";
+import {RouterModule} from "@angular/router";
 
 
 @NgModule({
   declarations: [BannerComponent],
     imports: [
         CommonModule,
-        SwiperModule
+        SwiperModule,
+        RouterModule
     ],
   exports: [BannerComponent]
 })
